f2103b1cb88fdc6cebba616b5fbce8f3505aea9b
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-12-14 19:48:31 +0600

    The Top Menu bar is redesigned with responsiveness

--- Job_Recommendation/tailwind.config.js
+++ Job_Recommendation/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {
       colors: {
         sidebarbgcolor: "#f7f7f7",
